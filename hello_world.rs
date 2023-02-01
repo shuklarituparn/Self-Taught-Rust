2164c6ad309c21190fceff4048ad9217b93c14a2
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-02-01 23:05:47 +0300

    added info about cargo

--- hello_world.rs
+++ hello_world.rs
@@ -2,7 +2,7 @@ fn main()  //Defining a function called main
 
 {
 
-    println!("Hello, World"); // ! means calling a macro. Without it we call a function 
+    println!("Hello, World"); // ! means calling a macro. Without it we call a function \
     println!("Hello, Galaxy");
     println!("Hello, Universe");
 }
