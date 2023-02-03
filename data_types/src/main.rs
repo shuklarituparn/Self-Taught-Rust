7226ad9b8cc09232832f24a6584631d192357b82
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-02-03 15:27:17 +0300

    fixed formatting issues

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -17,8 +17,8 @@ Length	Signed	Unsigned
 16-bit	i16	      u16
 32-bit	i32	      u32
 64-bit	i64	      u64
-128-bit	i128	  u128
-arch	isize	  usize
+128-bit	i128      u128
+arch	isize     usize
  */
 
 /*
