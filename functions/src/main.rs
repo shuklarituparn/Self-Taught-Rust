55c93712e711e887cfd647c230f748f1e5e6226d
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-02-03 10:26:39 +0300

    Added info about functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -25,6 +25,7 @@ fn another_function(x: i32) //necessary to indicate type
 
 //***************************Multiple Parameters*************************************//
 
+/*
 fn main() {
     print_labeled_measurement(5, 'h');
 }
@@ -32,5 +33,13 @@ fn main() {
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
 }
+*/
 
 
+
+//Statements are instructions that perform some action and do not return a value.
+//Expressions evaluate to a resultant value. Let’s look at some examples.
+fn main() {
+    let y = 6;
+    println!("Here Y={y} is a statement as it's not returning a value: ");
+}
