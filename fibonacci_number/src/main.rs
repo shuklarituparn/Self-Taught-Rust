23dc79ca63f87edbd01cd45cf480feb07ca237a2
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-02-04 22:00:28 +0300

    added more info

--- fibonacci_number/src/main.rs
+++ fibonacci_number/src/main.rs
@@ -14,7 +14,7 @@ fn main(){
     println!("Enter the term of the fibonacci sequence you want to find: ");
     let mut user_input = String::new();
 
-    io::stdin()
+    io::stdin() //handle to standard input stream of process
         .read_line(&mut user_input)
         .expect("Failed to read line");
 
@@ -22,3 +22,17 @@ fn main(){
 
     println!("fibonacci number is: {}", fibonacci(my_int));
 }
+/*
+BufRead is a type of Reader which has an internal buffer, allowing it to perform extra ways of reading.
+
+For example, reading line-by-line is inefficient without using a buffer, so if you want to read by
+line, you’ll need BufRead, which includes a read_line method as well as a lines iterator
+ */
+
+/*
+Note that you cannot use the ? operator in functions that do not return a Result<T, E>.
+Instead, you can call .unwrap() or match on the return value to catch any possible errors:
+ */
+
+//parse converts the string into int.
+//trim removes all the whitespaces
