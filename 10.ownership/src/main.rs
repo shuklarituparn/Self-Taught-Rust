1f8d6015d8d628ec467e67fc076957486a9ae544
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-03-07 22:49:12 +0300

    added about scopes

--- 10.ownership/src/main.rs
+++ 10.ownership/src/main.rs
@@ -14,7 +14,19 @@ string1();
 string2();
 string3();
 
-}
+takes_ownership(s);             // s's value moves into the function...
+                                            // ... and so is no longer valid here
+
+let x = 5;                      // x comes into scope
+
+makes_copy(x);                     // x would move into the function,
+                                                // but i32 is Copy, so it's okay to still
+                                                // use x afterward
+
+} // Here, x goes out of scope, then s. But because s's value was moved, nothing
+// special happens.
+
+
 
 /*
 
@@ -59,7 +71,7 @@ Something similar happens with the strings.
  */
 
  fn string2() {
-    let s1 = String::from("Hey");
+    let s1 = String::from("Hey\n");
     let s2 = s1;
     print!("{}", s2); //we can see that the variable s2 holds the string that belonged to the variable s1
 
@@ -94,3 +106,40 @@ fn string3() {
 
     println!("s1 = {}, s2 = {}", s1, s2);
 }
+
+/********************************************************************/ /*Stack-Only Data: Copy****************************************** */// 
+
+/*
+fn main() {
+    let x = 5; 
+    let y = x;
+
+    println!("x = {}, y = {}", x, y);
+}
+
+In this case we didn't have to call the copy function or the clone as the compiler knows the size of the int on the stack so it is easy to make a copy and we don't 
+have to actually make a deep copy as it doesn't makes sense.
+
+ */
+
+/*
+what types implement the Copy trait? You can check the documentation for the given type to be sure, but as a general rule, any group of simple scalar values 
+can implement Copy, and nothing that requires allocation or is some form of resource can implement Copy. Here are some of the types that implement Copy:
+
+    All the integer types, such as u32.
+    The Boolean type, bool, with values true and false.
+    All the floating-point types, such as f64.
+    The character type, char.
+    Tuples, if they only contain types that also implement Copy. For example, (i32, i32) implements Copy, but (i32, String) does not.
+
+ */
+
+
+fn takes_ownership(some_string: String) {       // some_string comes into scope. This function accepts a variable of type string_from
+    println!("{}", some_string);                
+}                                              // Here, some_string goes out of scope and `drop` is called. The backing
+                                              // memory is freed.
+
+fn makes_copy(some_integer: i32) {              // some_integer comes into scope
+    println!("{}", some_integer);
+}                                              // Here, some_integer goes out of scope. Nothing special happens.
