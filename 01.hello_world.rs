4f9e9f33a39d180487e83390753a9d82e239ae7b
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-03-07 21:49:09 +0300

    arranged the code

--- 01.hello_world.rs
+++
@@ -1,8 +0,0 @@
-fn main()  //Defining a function called main
-
-{
-
-    println!("Hello, World"); // ! means calling a macro. Without it we call a function \
-    println!("Hello, Galaxy");
-    println!("Hello, Universe");
-}
