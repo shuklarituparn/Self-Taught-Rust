a235d30dd0e626a80dad043c6d7936f188fab547
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-03-08 10:56:57 +0300

    updated ownership

--- 10.ownership/src/main.rs
+++ 10.ownership/src/main.rs
@@ -14,6 +14,12 @@ string1();
 string2();
 string3();
 
+let s3 = String::from("hello");
+
+let (s3, len) = calculate_length(s3); //Actually rust can return 2 values.
+
+println!("The length of '{}' is {}.", s3, len); 
+
 takes_ownership(s);             // s's value moves into the function...
                                             // ... and so is no longer valid here
 
@@ -23,8 +29,22 @@ makes_copy(x);                     // x would move into the function,
                                                 // but i32 is Copy, so it's okay to still
                                                 // use x afterward
 
-} // Here, x goes out of scope, then s. But because s's value was moved, nothing
-// special happens.
+let s1 = gives_ownership();         // gives_ownership moves its return
+                                                // value into s1
+print!("{}",s1);
+
+
+let s2 = String::from("hello");         // s2 comes into scope
+println!("{}",s2);
+        
+let s3 = takes_and_gives_back(s2);  // s2 is moved into
+println!("{}", s3);                                                    // takes_and_gives_back, which also
+                                                    // moves its return value into s3
+                                                    // Here, s3 goes out of scope and is dropped. s2 was moved, so nothing
+                                                    // happens. s1 goes out of scope and is dropped.
+        
+}                                               // Here, x goes out of scope, then s. But because s's value was moved, nothing
+                                                // special happens.
 
 
 
@@ -143,3 +163,33 @@ fn takes_ownership(some_string: String) {       // some_string comes into scope.
 fn makes_copy(some_integer: i32) {              // some_integer comes into scope
     println!("{}", some_integer);
 }                                              // Here, some_integer goes out of scope. Nothing special happens.
+
+//************************************************************************// Return Value and Scopes //**********************************************************/
+
+//Returning values can also transfer ownership. 
+
+   
+fn gives_ownership() -> String {             // gives_ownership will move its
+                                             // return value into the function
+                                             // that calls it
+
+    let some_string = String::from("yours"); // some_string comes into scope
+
+    some_string                              // some_string is returned and
+                                             // moves out to the calling
+                                             // function
+}
+
+// This function takes a String and returns one
+fn takes_and_gives_back(a_string: String) -> String { // Takes a variable of the type string_from type and returns it.
+
+                                                      
+
+    a_string  // a_string is returned and moves out to the calling function
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); // len() returns the length of a String
+
+    (s, length)
+}
