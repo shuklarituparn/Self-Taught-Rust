3bcdda6e30d4448b9cbb75414ae6b1d061ac2746
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-03-12 18:04:57 +0300

    added match

--- 19.match-construct/src/main.rs
+++ 19.match-construct/src/main.rs
@@ -1,3 +1,7 @@
+#![allow(unused)]
+
+
+
 #[derive(Debug)]
 enum UsState {
     Alabama, //enum takes a value of U.S. states 
@@ -26,6 +30,33 @@ fn value_in_cents(coin: Coin) -> u8 {
 
 fn main() {
     value_in_cents(Coin::Quarter(UsState::Alaska)); //calling that enum
+
+    fn plus_one(x: Option<i32>) -> Option<i32> { //Matching with Option<T>
+        match x {                                   //Match is exhaustive it should have a compare case of all possible values
+            None => None,      //if the input is not a number doing nothing . Match should cover all test cases so the "NONE" is necessary here
+            Some(i) => Some(i + 1),    //if the input is integer adding +1
+        }
+    }
+
+    let five = Some(5);     
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),  //
+        7 => remove_fancy_hat(),
+        other => move_player(other),//catch-all condition to catch all the remaining cases. Should be in end as match goes one by one to all cases
+        _ => (), //can also use "_"
+    }
+
+    fn add_fancy_hat() {}
+    fn remove_fancy_hat() {}
+    fn move_player(num_spaces: u8) {}
+
+
+
+
 }
 /*
 
@@ -34,3 +65,4 @@ code based on which pattern matches.
 
 With if the condition needs to be evaluated to  a boolean while with Match it can match to any value 
  */
+
