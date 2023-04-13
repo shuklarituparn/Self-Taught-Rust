6c7e448e80117b64597191cdd855d3730504a2d2
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-04-13 21:25:47 +0300

    added about lib

---
+++ 20.if_let/target/debug/deps/if_let-062888a9a1fd8082.d
@@ -0,0 +1,5 @@
+/home/rituparn/Documents/dev/languages/Rust/20.if_let/target/debug/deps/if_let-062888a9a1fd8082.rmeta: src/main.rs
+
+/home/rituparn/Documents/dev/languages/Rust/20.if_let/target/debug/deps/if_let-062888a9a1fd8082.d: src/main.rs
+
+src/main.rs:
---
+++ 21.packages_and_Crates/target/debug/deps/packages_and_Crates-72d1649bcfcf9ff4.d
@@ -0,0 +1,5 @@
+/home/rituparn/Documents/dev/languages/Rust/21.packages_and_Crates/target/debug/deps/packages_and_Crates-72d1649bcfcf9ff4.rmeta: src/main.rs
+
+/home/rituparn/Documents/dev/languages/Rust/21.packages_and_Crates/target/debug/deps/packages_and_Crates-72d1649bcfcf9ff4.d: src/main.rs
+
+src/main.rs:
